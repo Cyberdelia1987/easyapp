e3bf21f15dcd6decf610eee502feea3d004fa732
Cyber <cyberdelia1987@gmail.com>
2013-03-17 21:55:04 +0200

    Chart changes (from Highstock to Highcharts) because of Highstock does not support custom xAxis categories values

--- app/modules/main/libraries/serie/xaxis.php
+++ app/modules/main/libraries/serie/xaxis.php
@@ -8,7 +8,7 @@ class Lib_Main_Serie_XAxis extends Lib_Main_Serie
 	 * Проверяет, если порядок следования значений по оси X - прямой или обратный
 	 * @return bool
 	 */
-	public function isDIrectOrder()
+	public function isDirectOrder()
 	{
 		if ($this->_data[0] < $this->_data[1]) return true;
 		return false;
